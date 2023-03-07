98bc95f6df9db67b0979d5c634d41ecde7879c29
Deividas Ovsianikovas <deividas.ovsianikovas.2020@mumail.ie>
2023-03-07 17:02:34 +0000

    finishes fetching data

--- dynamic_data_creator/fetch_weather.py
+++ dynamic_data_creator/fetch_weather.py
@@ -1,28 +1,32 @@
-# import meteireann
-from metEireann import weatherData
+import meteireann
 import asyncio
 import datetime
 import pytz
+from meteostat import Hourly, Stations, Point
 
 import pandas as pd
 
-weather_data = weatherData.WeatherData()
+weather_data = meteireann.WeatherData()
 
 
-async def fetch_historical_data():
-    """Fetch data from API - (current weather and forecast)."""
-    await weather_data.fetch_data()
+# Fantastic weather API -> https://meteostat.net/en/place/ie/dublin?s=03969&t=2023-02-21/2023-02-28
 
-    date = datetime.datetime.now(pytz.utc).replace(
-        minute=0, second=0, microsecond=0)
+# TODO: Make sure to also include todays weather!
 
-    current_weather_data = weather_data.get_weather(date, max_hour=12)
+async def fetch_historical_data(startTime, endTime):
+    # Have to use a diffenent API to fetch historical data, met doesnt provide it
+    stations = Stations()
+    stations = stations.nearby(53.34, -6.25)
+    station = stations.fetch(1)
 
-    print('current:', current_weather_data)
+    data = Hourly(station, startTime, endTime)
+    data = data.fetch()
+
+    return extract_relevant_weather_from_meteostat(data)
 
 
 async def fetch_forecasts(days_to_forecast=7):
-    await weather_data.fetch_data()
+    await weather_data.fetching_data()
     today = datetime.datetime.now(pytz.utc).replace(
         hour=10, minute=0, second=0, microsecond=0) + datetime.timedelta(days=1)
 
@@ -33,29 +37,56 @@ async def fetch_forecasts(days_to_forecast=7):
             times.append(today.replace(
                 hour=h) + datetime.timedelta(days=i))
 
-    extract_relevant_weather(
+    return extract_relevant_weather_from_met(
         [weather_data.get_weather(time, hourly=True) for time in times])
 
 
-def extract_relevant_weather(weatherJson):
+# Re-write to use the meteostat API
+def extract_relevant_weather_from_met(weatherJson):
     df = pd.DataFrame(columns=["timestamp", "temperature", "rain"])
 
     for i in range(0, len(weatherJson)):
         weather = weatherJson[i]
         if weather is not None:
-            # print(weather)
+            print(weather)
             df = df.append({
                 "timestamp": weather["datetime"],
                 "temperature": weather["temperature"],
                 "rain": weather["precipitation"]
             }, ignore_index=True)
 
-    print(df)
+    # interpolate any nan temperatures, stick to 1 decimal place
+    df["temperature"] = df["temperature"].interpolate().round(1)
+
+    return df
+
+
+def extract_relevant_weather_from_meteostat(weatherDf):
+    df = pd.DataFrame(columns=["timestamp", "temperature", "rain"])
+
+    weatherDf = pd.DataFrame(weatherDf)
+
+    df["temperature"] = weatherDf["temp"]
+    df["rain"] = weatherDf["prcp"]
+    df["timestamp"] = weatherDf.index
+
+    # reset the index
+    df = df.reset_index(drop=True)
+
+    df = df[df["timestamp"].dt.hour.between(10, 18)]
+
+    return df
 
 
 async def main():
-    await fetch_historical_data()
-    await fetch_forecasts()
+    startTime = datetime.datetime(2023, 1, 1)
+    endTime = datetime.datetime(2023, 1, 10)
+
+    historical_data = await fetch_historical_data(startTime, endTime)
+    forecasted_data = await fetch_forecasts()
+
+    print(forecasted_data)
+    print(historical_data)
     await weather_data.close_session()
 
 if __name__ == "__main__":
