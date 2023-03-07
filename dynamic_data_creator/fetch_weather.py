9b80c330194b0db773b74bb30f6b01c687f48e93
Deividas Ovsianikovas <deividas.ovsianikovas.2020@mumail.ie>
2023-03-07 15:41:59 +0000

    modifying metEireann weather pip package

--- dynamic_data_creator/fetch_weather.py
+++ dynamic_data_creator/fetch_weather.py
@@ -1,34 +1,62 @@
-import meteireann
+# import meteireann
+from metEireann import weatherData
 import asyncio
 import datetime
+import pytz
 
-weather_data = meteireann.WeatherData()
-warning_data = meteireann.WarningData()
+import pandas as pd
+
+weather_data = weatherData.WeatherData()
 
 
-async def fetch_data():
+async def fetch_historical_data():
     """Fetch data from API - (current weather and forecast)."""
     await weather_data.fetching_data()
-    current_weather_data = weather_data.get_current_weather()
-    # print('current:', current_weather_data)
 
-    # await warning_data.fetching_data()
-    # current_warning_data = warning_data.get_warnings()
-    # print('warnings:', current_warning_data)
+    date = datetime.datetime.now(pytz.utc).replace(
+        minute=0, second=0, microsecond=0)
 
-    time_zone = datetime.datetime.now(
-        datetime.timezone.utc).astimezone().tzinfo
-    # daily_forecast = weather_data.get_forecast(time_zone, False)
-    # print('daily:', daily_forecast)
-    hourly_forecast = weather_data.get_forecast(time_zone, True)
-    print('hourly:', hourly_forecast)
-    return True
+    current_weather_data = weather_data.get_weather(date, max_hour=12)
+
+    print('current:', current_weather_data)
+
+
+async def fetch_forecasts(days_to_forecast=7):
+    await weather_data.fetching_data()
+    today = datetime.datetime.now(pytz.utc).replace(
+        hour=10, minute=0, second=0, microsecond=0)
+
+    # Add 1 hour to times until hitting 5 o clock, then incremenet the day
+    times = []
+    for i in range(0, days_to_forecast):
+        for h in range(10, 18):
+            times.append(today.replace(
+                hour=h) + datetime.timedelta(days=i))
+
+    extract_relevant_weather(
+        [weather_data.get_weather(time, hourly=True) for time in times])
+
+
+def extract_relevant_weather(weatherJson):
+    df = pd.DataFrame(columns=["timestamp", "temperature", "rain"])
+
+    for i in range(0, len(weatherJson)):
+        weather = weatherJson[i]
+        if weather is not None:
+            # print(weather)
+            df = df.append({
+                "timestamp": weather["datetime"],
+                "temperature": weather["temperature"],
+                "rain": weather["precipitation"]
+            }, ignore_index=True)
+
+    print(df)
 
 
 async def main():
-    await fetch_data()
+    await fetch_historical_data()
+    # await fetch_data()
     await weather_data.close_session()
-    await warning_data.close_session()
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
